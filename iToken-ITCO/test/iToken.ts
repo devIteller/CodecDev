1fbad9b6cb22e256ad05685b2e2d3eea6659632a
whanod <whanod@gmail.com>
2023-09-11 00:54:51 +0330

    Add token +tests

---
+++ iToken-ITCO/test/iToken.ts
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import hre, { ethers } from "hardhat";
+import { time } from "@nomicfoundation/hardhat-network-helpers";
+
+describe("Token Contract", function () {
+  it("Should Deploy Token Contract and Check Total Supply", async () => {
+    const [owner, rando] = await hre.ethers.getSigners();
+    let iToken = await ethers.deployContract("iToken");
+
+    let decimals = 10n ** (await iToken.decimals());
+    console.log(decimals);
+
+    let totalSupply = await iToken.totalSupply();
+    expect(totalSupply / decimals).to.equal(1000000000n);
+  });
+
+  it("Should Deploy Token Contract and name and Symbol", async () => {
+    const [owner, rando] = await hre.ethers.getSigners();
+    let iToken = await ethers.deployContract("iToken");
+    expect(await iToken.name()).to.equal("iToken")
+    expect(await iToken.symbol()).to.equal("ITCO")
+
+
+
+  })
+});
