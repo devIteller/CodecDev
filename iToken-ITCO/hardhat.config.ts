9bdaac49f8282f6add9bc103d4dde24b2e769d8c
whanod <whanod@gmail.com>
2023-09-11 23:33:07 +0330

    Change config & deploy file

--- iToken-ITCO/hardhat.config.ts
+++ iToken-ITCO/hardhat.config.ts
@@ -2,7 +2,18 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+  solidity:  {
+    version: '0.8.19',
+    settings: {
+      optimizer: { enabled: true, runs: 200 }}
+  
+},
+  
+
+    
 };
 
 export default config;
+
+
+
--- iToken-ITCO/test/iToken.ts
+++ iToken-ITCO/test/iToken.ts
@@ -5,12 +5,14 @@ import { time } from "@nomicfoundation/hardhat-network-helpers";
 describe("Token Contract", function () {
   it("Should Deploy Token Contract and Check Total Supply", async () => {
     const [owner, rando] = await hre.ethers.getSigners();
+    
     let iToken = await ethers.deployContract("iToken");
 
     let decimals = 10n ** (await iToken.decimals());
-    console.log(decimals);
+
 
     let totalSupply = await iToken.totalSupply();
+    console.log(totalSupply / decimals);
     expect(totalSupply / decimals).to.equal(1000000000n);
   });
 
